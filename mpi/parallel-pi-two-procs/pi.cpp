bb75c5904e3b61378961665ed0dec0b1d8da98d7
Elaine Swanson <eswan.star@gmail.com>
2024-06-26 19:57:57 +0300

    Here is all of my crap

--- mpi/parallel-pi-two-procs/pi.cpp
+++ mpi/parallel-pi-two-procs/pi.cpp
@@ -1,23 +1,52 @@
 #include <cstdio>
 #include <cmath>
+#include <mpi.h>
 
-constexpr int n = 840;
+constexpr int n = 10000;
+int rank, ntasks;
+MPI_Status status;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
 
-  printf("Computing approximation to pi with N=%d\n", n);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  int istart = 1;
-  int istop = n;
+  double rankzero = 0.0, rankone = 0.0;
 
-  double pi = 0.0;
-  for (int i=istart; i <= istop; i++) {
-    double x = (i - 0.5) / n;
-    pi += 1.0 / (1.0 + x*x);
+  if (rank == 0){
+  	printf("Computing approximation to pi with first rank up to N=%d\n", n/2);
+
+  	int istart = 1;
+  	int istop = n/2;
+
+  	for (int i=istart; i <= istop; i++) {
+    		double x = (i - 0.5) / n;
+    		rankzero += 1.0 / (1.0 + x*x);
+  	}
+  
+ 	MPI_Recv(&rankone, 1, MPI_DOUBLE, 1, 52, MPI_COMM_WORLD, &status);
+
+	double pi = rankzero + rankone;
+	pi *= 4.0/n;
+        printf("Approximate pi=%18.16f (exact pi=%10.8f)\n", pi, M_PI);
+	
   }
 
-  pi *= 4.0 / n;
-  printf("Approximate pi=%18.16f (exact pi=%10.8f)\n", pi, M_PI);
+  if (rank == 1){
+ 	printf("Computing approximation to pi with second rank up N=%d\n", n);
 
-}
+	int istart = (n/2) + 1;
+        int istop = n;
+
+        for (int i=istart; i <= istop; i++) {
+                double x = (i - 0.5) / n;
+                rankone += 1.0 / (1.0 + x*x);
+        }
+
+  	MPI_Send(&rankone, 1, MPI_DOUBLE, 0, 52, MPI_COMM_WORLD);
+  }
+  
+  MPI_Finalize();	
+
+  }
